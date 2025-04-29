e1e0b4a39a355b92f373e78c562372b571180ecf
Miladnk79 <milad.nk@gmail.com>
2025-04-29 15:35:39 +0330

    update date time in requests, update size of textbox

--- models.py
+++ models.py
@@ -1,8 +1,14 @@
-from datetime import datetime
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+import jdatetime
 
 db = SQLAlchemy()
+def calculateTime():
+    today = datetime.datetime.now()
+    todaystr = str(today)
+    data = datetime.datetime(int(todaystr[:4]), int(todaystr[5:7]), int(todaystr[8:10]),int(todaystr[11:13]), int(todaystr[14:16]), int(todaystr[17:19]))
+    return jdatetime.datetime.fromgregorian(date=data)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,4 +25,6 @@ class Request(db.Model):
     filename = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.String(20), default='pending')  # New status field
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)  # New date field
+    date_created = db.Column(db.DateTime, default=calculateTime())  # New date field
+
+
