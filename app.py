54aa448e12bb39fb1d8d984a6385ef1d953432ae
Milad Soltanzadeh <65998225+Miladnk79@users.noreply.github.com>
2025-06-03 12:54:20 +0330

    diactive actions when request status equals to Done

--- app.py
+++ app.py
@@ -240,5 +240,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
 #    app.run(debug=True, port=5000)
-    app.run(debug=False, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
 
